47996cb8a0092d76af8045c6ccff676f5cdb4817
bartlomiejmusial <bartlomiej.musial.dev@gmail.com>
2022-02-17 14:56:19 +0100

    RWD, spacing refactor

--- src/components/EventDescriptionCard.js
+++ src/components/EventDescriptionCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Card, Paragraph } from 'flotiq-components-react';
 
-const EventDescriptionCard = ({ name, headerImage, date, description, address, price }) => (
+const EventDescriptionCard = ({ name, headerImage, date, description }) => (
     <Card
         vertical
         bordered={false}
         rounded="none"
-        additionalClasses={['w-full flex flex-col mb-10 my-10']}
+        additionalClasses={['w-full flex flex-col mb-10 my-10 rounded-lg']}
     >
         <Card.Img
             src={headerImage}
@@ -15,24 +15,19 @@ const EventDescriptionCard = ({ name, headerImage, date, description, address, p
         />
         <Card.Body
             additionalClasses={[
-                'grid grid-cols-4 border-2 border-blue !p-0',
+                'grid grid-cols-4 border-2 border-blue !p-0 rounded-b-lg',
             ]}
         >
-            <div className="pt-24 pb-12 px-10">
-                <p className="flex items-start justify-center h-full border-r-2 border-gray">
+            <div className="pt-24 pb-12">
+                <p className="flex items-start justify-center h-full border-r-2 border-gray text-lg px-5 md:px-0">
                     {date}
                 </p>
             </div>
-            <div className="col-span-3 py-10">
+            <div className="col-span-3 py-10 px-5 md:px-10">
                 <Card.Title additionalClasses={['font-normal mt-5 mb-8']}>
                     {name}
                 </Card.Title>
                 <Paragraph text={description} />
-                <div className="mt-10 flex flex-wrap items-center justify-self-end justify-between">
-                    <p>{address}</p>
-                    <p>{price}</p>
-                </div>
-
             </div>
         </Card.Body>
     </Card>
